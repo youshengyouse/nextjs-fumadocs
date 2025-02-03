8ea1b67a6ff5a8c1c7cbbe882fd7c64fc4ab7b07
Fuma Nama <god63820869@gmail.com>
2025-02-03 20:35:42 +0800

    update to Tailwind CSS v4, Fumadocs v15

--- app/docs/layout.client.tsx
+++ app/docs/layout.client.tsx
@@ -8,14 +8,14 @@ export function Body({ children }: { children: ReactNode }) {
   if (!slug) return children;
   let color: string | undefined;
 
-  if (slug[0] === "app") color = `221.21 83.19% 65.33%`;
-  else if (slug[0] === "pages") color = "271.48 81.33% 65.88%";
+  if (slug[0] === "app") color = `hsl(221.21 83.19% 65.33%)`;
+  else if (slug[0] === "pages") color = "hsl(271.48 81.33% 65.88%)";
   return (
     <>
       {color ? (
         <style>
           {`:root {
-            --fd-primary: ${color};
+            --color-fd-primary: ${color};
         }`}
         </style>
       ) : null}
