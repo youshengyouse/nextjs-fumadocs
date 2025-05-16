017827563af4702e0ab586f5fd3db1228a34dff6
youshengyouse <19722008@163.com>
2025-05-16 14:52:53 +0800

    首页翻译成中文

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-xl font-semibold">Next.js Documentation</h1>
+      <h1 className="mb-4 text-xl font-semibold">Next.js 文档</h1>
       <p className="text-fd-muted-foreground mb-4">
-        Preview the Next.js docs with Fumadocs
+      使用 Fumadocs 预览 Next.js 文档
       </p>
       <div className="flex flex-row items-center justify-center">
         <Link
           href="/docs"
           className="rounded-full text-fd-primary-foreground font-medium text-sm bg-fd-primary px-6 py-2.5 transition-colors hover:bg-fd-accent hover:text-fd-accent-foreground"
         >
-          Open Docs
+          打开文档
         </Link>
       </div>
     </main>
